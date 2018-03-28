09697d70f404e8361d5210114c4526178bd303c7
Rahul Samaddar <samaddarr@HCL.COM>
2018-03-28 16:34:43 +0530

    added new temporary window

---
+++ app/js/windows/newbiblewindow.js
@@ -0,0 +1,45 @@
+sofia.config = $.extend(sofia.config, {
+	enableNewBibleWindow: true,
+	NewBibleWindowDefaultBibleFragmentid: 'JN1_1',
+	NewBibleWindowDefaultBibleVersion: 'deaf_ASESLV'
+});
+
+var NewBibleWindow = function(id, node, init_data) {
+
+	var window =  new TextWindow(id, node, init_data, 'newbible');
+
+	node.node.on('click', '.deaf-video-header input', function() {
+		var button = $(this),
+			url = button.attr('data-src'),
+			video = button.closest('.deaf-video').find('video');
+
+		button
+			.addClass('active')
+			.siblings()
+				.removeClass('active');
+
+		console.log(url, video);
+
+		video.attr('src', url);
+	});
+
+	return window;
+};
+
+sofia.initMethods.push(function() {
+
+	if (sofia.config.enableNewBibleWindow)	{
+		sofia.windowTypes.push({
+			className:'NewBibleWindow',
+			param: 'newbible',
+			paramKeys: {
+				'textid': 't',
+				'fragmentid':'v'
+			},
+			init: {
+				'textid': sofia.config.NewBibleWindowDefaultBibleVersion,
+				'fragmentid': sofia.config.NewBibleWindowDefaultBibleFragmentid
+			}
+		});
+	}
+});
