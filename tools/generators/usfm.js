401eabfbbdadeba902f3004455f6799ea86ef0b7
uday <udkumar@hotmail.com>
2018-06-22 10:27:57 +0530

    correct code for images

--- tools/generators/usfm.js
+++ tools/generators/usfm.js
@@ -23,7 +23,7 @@ function generate(inputBasePath, info, createIndex, startProgress, updateProgres
 			chapterData: [],
 			indexData: {},
 			indexLemmaData: {},
-			aboutHtml: fs.existsSync(aboutPath) ? fs.readFileSync(path.join(inputPath, 'about.html'), 'utf8') : ''
+			aboutHtml: fs.existsSync(aboutPath) ? fs.readFileSync(path.join(inputBasePath, 'about.html'), 'utf8') : ''
 		}
 
 	skipAheadKeys = ['', 'add', 'add*', 'wj', 'wj*', 'x', 'x*', 'f', 'f*', 'qs', 'ft', 'bk', 'fqa'];
