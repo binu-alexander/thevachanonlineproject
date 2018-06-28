e4aff9a682315db6cf4204a1581d07a968530c65
uday <udkumar@hotmail.com>
2018-06-28 17:45:26 +0530

    default hindi notes

--- app/js/core/config-default.js
+++ app/js/core/config-default.js
@@ -10,7 +10,7 @@ sofia.config = {
 	windows: [
 		{type: 'bible', data: {textid: 'hindi_irv', fragmentid: 'JN3_16'}},
 		{type: 'bible', data: {textid: 'english_ult', fragmentid: 'JN3_16'}},
-		{type: 'commentary', data: {textid: 'comm_eng_tske', fragmentid: 'JN3_16'}},
+		{type: 'commentary', data: {textid: 'comm_hin_irv', fragmentid: 'JN3_16'}},
 	],
 
 	// URL to content
