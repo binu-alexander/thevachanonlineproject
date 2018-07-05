61ea13ed24ed50f2e6f32a0b80377f00009a03ae
uday <udkumar@hotmail.com>
2018-07-05 20:38:05 +0530

    remove more code form textwindows

--- app/js/windows/textwindow.js
+++ app/js/windows/textwindow.js
@@ -517,25 +517,4 @@ sofia.initMethods.push(function() {
 	}
 });
 
-var MoreWindow = function(id, node, init_data) {
-	// return new TextWindow(id, node, init_data, 'tnotes');
-};
 
-sofia.initMethods.push(function() {
-
-	if (typeof sofia.config.newTnotesWindowTextId != 'undefined') {
-
-		sofia.windowTypes.push({
-			className:'MoreWindow',
-			param: 'more',
-			paramKeys: {
-				'textid': 't',
-				'fragmentid':'v'
-			},
-			init: {
-				'textid':sofia.config.newTnotesWindowTextId,
-				'fragmentid':''
-			}
-		});
-	}
-});
